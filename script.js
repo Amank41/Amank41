078a38a42bb46166e241f962004b8c8b856b14af
Amank41 <amankushwahaa1999@gmail.com>
2024-09-26 10:30:05 +0530

    Amank41
    Share

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector('.instagram-btn').addEventListener('click', () => {
+    // Action on Instagram button click
+    console.log('Instagram button clicked!');
+  });
+  
+  document.querySelector('.telegram-btn').addEventListener('click', () => {
+    // Action on Telegram button click
+    console.log('Telegram button clicked!');
+  });
+  
+  document.querySelector('.register-btn').addEventListener('click', () => {
+    // Action on Register button click
+    console.log('Register button clicked!');
+  });
+  
+  document.querySelector('.goagame-btn').addEventListener('click', () => {
+    // Action on GoaGame button click
+    console.log('GoaGame button clicked!');
+  });
